04e4d0eb764058e3d38aeefab0708e42b3c87ac2
Greg Trent <pyromaster4@gmail.com>
2015-01-09 15:18:04 -0700

    Added acknowledgments for help

--- plot3.R
+++ plot3.R
@@ -1,5 +1,6 @@
 setwd("/home/greg/Documents/MOOC/Coursera/EDA/ExData_Plotting1.git/trunk")
 power <- read.csv('household_power_consumption.txt', sep=";", stringsAsFactors=FALSE)
+#Line to get $DateTime provided by Dino Nienhold via Discussion Forums
 power$DateTime <- as.POSIXct(paste(power$Date, power$Time), format="%d/%m/%Y %H:%M:%S")
 power$Date <- as.Date(power$Date, format="%d/%m/%Y")
 power <- subset(power, power$Date >= '2007-02-01' & power$Date <= '2007-02-02')
